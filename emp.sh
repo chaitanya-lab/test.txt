4f74c38e6274cafabf0272b5c307df375f5923dc
unknown <you@example.com>
2020-11-21 12:32:58 +0530

    emp file updated

--- emp.sh
+++ emp.sh
@@ -6,3 +6,32 @@ then
 else
 	echo "employee is not present"
 fi
+read a
+isPartTime=1;
+isFullTime=2;
+empRatePerHr=100;
+randomCheck=$(($RANDOM%3));
+case $a in
+        1)
+                if [ $isFullTime -eq $randomCheck ]
+                then
+                empHrs=8;
+                fi
+                ;;
+        2)
+                if [ $isPartTime -eq $randomCheck ]
+                then
+                empHrs=4;
+                fi
+                ;;
+        3)
+                empHrs=0;
+                ;;
+        *)
+                echo "default"
+                ;;
+        esac
+
+salary=$(($empHrs*$empRatePerHr))
+echo "$salary"
+
